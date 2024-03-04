4236c4cfb1fa0c414a371512c1ef00be1cca5cd5
e.dyakov <e3001dk@gmail.com>
2024-03-04 22:05:14 +0200

    header test-3

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 'use strict'
 
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+let vh = window.innerHeight * 0.01
+document.documentElement.style.setProperty('--vh', `${vh}px`)
+
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01
+  document.documentElement.style.setProperty('--vh', `${vh}px`)
+})
 
 const header = document.querySelector('.header')
 const logo = header.querySelector('.header__logo')
